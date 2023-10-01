1b04bb8bf5eb68523846a8c6c1e526df2cc619af
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-10-01 18:50:55 +0300

    Backbone of algorithm

---
+++ lab4/lab4/KAverage.cs
@@ -0,0 +1,79 @@
+﻿namespace lab4
+{
+    public class KAverage
+    {
+        public int[] Cores;
+
+        public int[][] Elements;
+
+
+        public KAverage()
+        {
+
+        }
+
+
+        public int[][] Get_DividedClasses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[][] Get_BestClusterCore()
+        {
+/*            var bestCenter = new Point((int)Dots.Average(x => x.TopLeft.X), (int)Dots.Average(x => x.TopLeft.Y));
+            var minDifferent = double.MaxValue;
+            var minDifferentPoint = new Point();
+
+            foreach (var centerCandidate in Dots)
+            {
+                var different = GetDotsDistance(bestCenter, centerCandidate.TopLeft);
+                if (!(different < minDifferent))
+                {
+                    continue;
+                }
+                minDifferent = different;
+                minDifferentPoint = centerCandidate.TopLeft;
+            }
+
+            return minDifferentPoint;*/
+            throw new NotImplementedException();
+        }
+
+        protected static double Get_ElementsDistance(int firstElement, int secondElement)
+        {/*
+            var xDifferent = firstDot.X - secondDot.X;
+            var yDifferent = firstDot.Y - secondDot.Y;
+            return Math.Sqrt(xDifferent * xDifferent + yDifferent * yDifferent);*/
+            throw new NotImplementedException();
+        }
+
+        public static void FindCluster()
+        {
+            /*            int ind = 0;
+                        int minDistance = CountDistance(TopLeft, clusters[ind].Center);
+                        for (int i = 1; i < clusters.Count; i++)
+                        {
+                            int currentDistance = CountDistance(TopLeft, clusters[i].Center);
+                            if (minDistance > currentDistance)
+                            {
+                                minDistance = currentDistance;
+                                ind = i;
+                            }
+                        }
+
+                        Pen.Color = clusters[ind].Color;
+
+                        clusters[ind].Dots.Add(this);*/
+            throw new NotImplementedException();
+        }
+
+        private static int CountDistance(Point p1, Point p2)
+        {
+            /*            var a = Math.Pow((p1.X - p2.X + WIDTH / 2), 2);
+                        var b = Math.Pow((p1.Y - p2.Y + HEIGHT / 2), 2);
+                        int res = (int)Math.Sqrt(a + b);
+                        return res;*/
+            throw new NotImplementedException();
+        }
+    }
+}
--- lab4/lab4/Perceptron.cs
+++ lab4/lab4/Perceptron.cs
@@ -1,7 +1,35 @@
 ﻿namespace lab4
 {
-    public class Perceptron
+    public sealed class Perceptron
     {
+        // к образу добавит в конец 1
+        private int[][] weightMatrices;
+        private int[][] objects;
 
+        public Perceptron(int classesNum, int distinclionsNum)
+        {
+
+        }
+
+        public int[][] Get_SepartingFunctions()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int[][] Generate_Sample()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Initialize_Classes()
+        {
+            throw new NotImplementedException ();
+        }
+
+        private void Update_WeightMatrices()
+        {
+            throw new NotImplementedException();
+        }
+       
     }
 }
