e80e53f48af9af46e89833fff2834fb53dcacd08
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-10-02 09:59:43 +0300

    Perceptron started

--- lab4/lab4/Perceptron.cs
+++ lab4/lab4/Perceptron.cs
@@ -2,40 +2,60 @@
 {
     public sealed class Perceptron
     {
-        // к образу добавить в конец 1
         private List<List<int>> WeightMatrices;
         private List<Class> Classes;
 
+        private int _classesNum;
+        private int _objectsNum;
+        private int _distinctionsNum;
+
         public Perceptron(int classesNum, int objectsNum, int distinctionsNum)
         {
+            _classesNum = classesNum;
+            _objectsNum = objectsNum;
+            _distinctionsNum = distinctionsNum;
+
             WeightMatrices = new();
             for (int i = 0; i < classesNum; i++)
             {
                 WeightMatrices.Add(new List<int>());
             }
             Classes = KAverage.Get_DividedClasses(classesNum, objectsNum, distinctionsNum);
-            Console.WriteLine();
+            Extend_Objects();
+            Generate_WeightMatrices();
         }
 
-        public int[][] Get_SepartingFunctions()
+        public List<List<int>> Get_SepartingFunctions()
+        {
+            bool isAnotherUpdate = true;
+            while(isAnotherUpdate)
+            {
+                isAnotherUpdate = Update_WeightMatrices();
+            }
+
+            return Generate_SeparatingFunctions();
+        }
+
+        private void Generate_WeightMatrices()
         {
             throw new NotImplementedException();
         }
 
-        private int[][] Generate_Sample()
+        private void Extend_Objects()
+        {
+            _distinctionsNum++;
+            throw new NotImplementedException();
+        }
+
+        private bool Update_WeightMatrices()
         {
             throw new NotImplementedException();
         }
 
-        private void Initialize_Classes()
-        {
-            throw new NotImplementedException ();
-        }
-
-        private void Update_WeightMatrices()
+        private List<List<int>> Generate_SeparatingFunctions()
         {
             throw new NotImplementedException();
         }
-       
+
     }
 }
