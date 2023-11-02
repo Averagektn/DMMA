80bb428c6e4b4eb6d910ec0a9a3a2fee3a21ecf2
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-11-02 19:24:52 +0300

    Started perceptron

--- lab9/lab9/MainForm.Designer.cs
+++ lab9/lab9/MainForm.Designer.cs
@@ -28,16 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(465, 88);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 110);
+            button1.TabIndex = 0;
+            button1.Text = "Load image";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(423, 272);
+            button2.Name = "button2";
+            button2.Size = new Size(153, 88);
+            button2.TabIndex = 1;
+            button2.Text = "Verify vector";
+            button2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(984, 561);
+            ClientSize = new Size(784, 561);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            Margin = new Padding(4);
+            Margin = new Padding(4, 4, 4, 4);
             MaximizeBox = false;
             Name = "MainForm";
             Text = "Lab 9";
@@ -45,5 +67,8 @@
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
     }
 }
